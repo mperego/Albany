99391b587ca8ce7fe3d6d6a6b0ee1d73c608d11a
Mauro Perego <mperego@sandia.gov>
2018-12-10 12:19:48 -0700

    LandIce: add response to compute flux through grounding line

--- src/problems/Albany_ResponseUtilities_Def.hpp
+++ src/problems/Albany_ResponseUtilities_Def.hpp
@@ -20,6 +20,7 @@
 #ifdef ALBANY_LANDICE
 #include "LandIce_ResponseSurfaceVelocityMismatch.hpp"
 #include "LandIce_ResponseSMBMismatch.hpp"
+#include "LandIce_ResponseGLFlux.hpp"
 #include "LandIce_ResponseBoundarySquaredL2Norm.hpp"
 #endif
 #if defined(ALBANY_LCM)
@@ -159,6 +160,10 @@ Albany::ResponseUtilities<EvalT,Traits>::constructResponses(
   else if (responseName == "Surface Mass Balance Mismatch")
   {
     res_ev = rcp(new LandIce::ResponseSMBMismatch<EvalT,Traits>(*p,dl));
+  }
+  else if (responseName == "Grounding Line Flux")
+  {
+    res_ev = rcp(new LandIce::ResponseGLFlux<EvalT,Traits>(*p,dl));
   }
   else if (responseName == "Boundary Squared L2 Norm")
   {
